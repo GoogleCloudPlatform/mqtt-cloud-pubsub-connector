2d47ec990929da7c26ed49faf6ab85575bfb7be7
Marco Ferrari <ferrarim@google.com>
2025-09-24 08:26:09 +0200

    Revert "build(deps): bump hashicorp/google in /terraform-init (#896)" (#900)
    This reverts commit 1e46cc53baca7fe7cec2547362b16e8114637784.

--- terraform-init/versions.tf
+++ terraform-init/versions.tf
@@ -18,11 +18,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "7.4.0"
+      version = "7.3.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "7.4.0"
+      version = "7.3.0"
     }
   }
 }
